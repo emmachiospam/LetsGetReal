ac456eeb1b49d4982548a77658eee987d4e66fa7
emmachio <emmany2409@gmail.com>
2020-11-20 13:21:00 -0500

    created

---
+++ RealNumber.java
@@ -0,0 +1,20 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
+
+  // public boolean equals(RealNumber other) {
+  //
+  // }
+
+}
