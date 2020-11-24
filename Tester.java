fb5900cb9860ddf335c744762f076d1345ec8c6b
emmachio <emmany2409@gmail.com>
2020-11-24 02:29:57 -0500

    tested everything

---
+++ Tester.java
@@ -0,0 +1,24 @@
+public class Tester {
+
+  public static void main(String[] args) {
+    RationalNumber a = new RationalNumber(2,5);
+    RationalNumber b = new RationalNumber(1,15);
+    // System.out.println(a.multiply(b));
+    // RationalNumber test = new RationalNumber(12,0);
+    System.out.println(a.getValue());
+    System.out.println(a.subtract(b));
+    // System.out.println(a.getNumerator());
+    // System.out.println(a.getDenominator());
+    // RationalNumber b = new RationalNumber(5,-2);
+    // System.out.println(b.getValue());
+    // System.out.println(b.getNumerator());
+    // System.out.println(b.getDenominator());
+    // RationalNumber c = new RationalNumber(-5,-2);
+    // System.out.println(c.getValue());
+    // System.out.println(c.getNumerator());
+    // System.out.println(c.getDenominator());
+    // System.out.println(a.subtract(b));
+  }
+
+
+}
