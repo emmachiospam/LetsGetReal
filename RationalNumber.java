446003b8dddeebb04f85919da7386f4e8abf5b4b
emmachio <emmany2409@gmail.com>
2020-11-24 01:59:30 -0500

    gcd

--- RationalNumber.java
+++ RationalNumber.java
@@ -50,4 +50,13 @@ public class RationalNumber extends RealNumber
     return(numerator + "/" + denominator);
   }
 
+  public static int gcd(int a, int b) {
+    int gcd = 0;
+    for(int i = 1; i <= a && i <= b; i++) {
+          if(a % i == 0 && b % i == 0)
+              gcd = i;
+    }
+    return gcd;
+  }
+
 }
