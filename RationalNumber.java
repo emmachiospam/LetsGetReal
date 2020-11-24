65aa46c522c447bc1ff9cc1f86aae2e8c9075b84
emmachio <emmany2409@gmail.com>
2020-11-24 01:14:37 -0500

    fixed constructor

---
+++ RationalNumber.java
@@ -0,0 +1,38 @@
+public class RationalNumber extends RealNumber
+{
+  private int numerator, denominator;
+
+  public RationalNumber (int nume, int deno) {
+    super( ((double) nume) / deno);
+    if(deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    }
+    else if(deno < 0 && nume > 0) {
+      numerator = nume * -1;
+      denominator = deno * -1;
+    }
+    else if(deno < 0 && nume < 0) {
+      numerator = nume * -1;
+      denominator = deno * -1;
+    }
+    else{
+      numerator = nume;
+      denominator = deno;
+    }
+  }
+
+  public double getValue() {
+    double result = (double)numerator/denominator;
+    return result;
+  }
+
+  public int getNumerator() {
+    return numerator;
+  }
+
+  public int getDenominator() {
+    return denominator;
+  }
+
+}
