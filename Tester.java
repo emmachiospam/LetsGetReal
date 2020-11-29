6c08065d8c05b280e891a133ff7f4ad5320ce10f
emmachio <emmany2409@gmail.com>
2020-11-29 14:11:33 -0500

    tested

--- Tester.java
+++ Tester.java
@@ -1,12 +1,13 @@
 public class Tester {
 
   public static void main(String[] args) {
-    RationalNumber a = new RationalNumber(2,5);
-    RationalNumber b = new RationalNumber(1,15);
+    RationalNumber k = new RationalNumber(1,9);
+    RationalNumber n = new RationalNumber(-1,9);
+    System.out.println(k.add(n));
     // System.out.println(a.multiply(b));
     // RationalNumber test = new RationalNumber(12,0);
-    System.out.println(a.getValue());
-    System.out.println(a.subtract(b));
+    // System.out.println(a.getValue());
+    // System.out.println(a.compareTo(b));
     // System.out.println(a.getNumerator());
     // System.out.println(a.getDenominator());
     // RationalNumber b = new RationalNumber(5,-2);
@@ -17,7 +18,7 @@ public class Tester {
     // System.out.println(c.getValue());
     // System.out.println(c.getNumerator());
     // System.out.println(c.getDenominator());
-    // System.out.println(a.subtract(b));
+    // System.out.println(b.add(a));
   }
 
 
